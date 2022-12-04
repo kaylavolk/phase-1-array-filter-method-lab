6b51d11fc306d3b4775a9a2f2b9efe3053cbd4cb
Kayla Volk <kaylavolk8@gmail.com>
2022-12-04 14:46:26 -0500

    commit tree 48ba2b955595c3326cbd710fafada7bb1e13e166

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, string){
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+    }
+    function fuzzyMatch(drivers, string){
+        const lengthOfString = string.length
+        return drivers.filter(function(driver) {
+            return driver.slice(0, lengthOfString) === string
+        })
+    }
+    function matchName(drivers, name){
+        return drivers.filter(function(driver){
+          return driver.name === name
+        })
+      }
